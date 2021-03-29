d38abfc68c3bcd0e15f7ee22b235f834b354ed07
Ricardo Ardiles <ricardo.ardiles@pagofacil.cl>
2021-03-29 19:16:30 -0300

    ADD: configuration

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=rardiles
+export POSTGRES_PASSWORD=udacity123..
+export POSTGRES_HOST=udacity-dev.c4k86qizidvt.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udacity-229318163752-dev
+export AWS_REGION=us-west-2
+export AWS_PROFILE=default
+export JWT_SECRET=helloudacity
+export URL=http://localhost:8100
